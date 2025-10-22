57342d49b4cb0aeebe37c9cf20eab113d6f2f84e
Jonas Hietala <mail@jonashietala.se>
2025-10-22 14:25:50 +0200

    Copy ftplugin settings

---
+++ ftplugin/lua.lua
@@ -0,0 +1,10 @@
+local set = vim.opt_local
+
+set.shiftwidth = 2
+set.ts = 2
+set.sts = 2
+set.sw = 2
+set.expandtab = true
+
+-- Write and source lua file
+vim.keymap.set("n", "<leader>w", ":write<CR>:source %<CR>")
