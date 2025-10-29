9c06288746e76610b1d61897faee48d6c315efb9
Jonas Hietala <mail@jonashietala.se>
2025-10-29 15:44:07 +0100

    Proper workspace_folders for fennel-ls
    However, diagnostic errors remain

--- after/lsp/fennel_ls.lua
+++ after/lsp/fennel_ls.lua
@@ -1,4 +1,5 @@
-return {
-	-- This errors out for some reason...
-	-- workspace_folders = vim.api.nvim_get_runtime_file("fnl", true),
-}
+local workspace_folders_on_rtp = vim.tbl_map(function(path)
+	return { uri = "file://" .. path, name = vim.fs.basename(vim.fs.dirname(path)) }
+end, vim.api.nvim_get_runtime_file("fnl", true))
+
+return { workspace_folders = workspace_folders_on_rtp }
